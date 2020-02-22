6a0d76b4918408601ee3c4a3ba112d6258e6400d
kgrdzelidze <60541221+kgrdzelidze@users.noreply.github.com>
2020-02-22 12:55:10 +0400

    Add files via upload

---
+++ users.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2020 at 08:54 AM
+-- Server version: 8.0.18
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `scsa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `last_name` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `address` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `email` text CHARACTER SET utf8 COLLATE utf8_general_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `address`, `email`) VALUES
+(3, 'zako', 'janikashvili', 'dfsa', 'tinkivinki@gmail.com'),
+(4, 'tamta', 'turashvili', 'tdsa', 'tturashivli@gmail.com'),
+(5, 'andro', 'Jgharkava', 'rewa', 'ajgharkava@gmail.com'),
+(6, 'nodo', 'ugulava', 'fdsajk', 'nugulava@gmail.com'),
+(7, 'andro', 'bursulaia', 'fds', 'abursulaia@gmail.com'),
+(12, 'irma', 'chinchaladze', 'fdsa', 'ichinchaladze@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
